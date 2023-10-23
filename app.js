c9b5d280e2d5659ba0351cc8c68c0fde77d57669
kedarshenoy <akedarshenoy@gmail.com>
2023-10-23 14:49:59 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,38 @@
+const express =require('express')
+const routes =require('./Routes/index')
+const app =express();
+const mongose= require('mongoose')
+const port=5402;
+const bodyParser =require("body-parser")
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*'); 
+  res.setHeader('Access-Control-Allow-Methods', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
+
+app.use("/",routes);
+
+
+mongose.connect(
+  // 'mongodb://localhost:27017/leetcode',
+  'mongodb+srv://kedar:connectionpassword@cluster0.ufsetpy.mongodb.net/leetcode?retryWrites=true&w=majority',
+
+  {
+    useNewUrlParser:true,
+    useUnifiedTopology:true,
+  }
+).then(success=>
+  {console.log("connected to mongodb")
+  app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+  });
+}
+).catch(error=>{
+  console.log("error")
+})
+
+
